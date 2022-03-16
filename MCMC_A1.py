994d410b7288d65039fc28d8f06b599efbe01f05
gfiacco12 <gfiacco12@gmail.com>
2022-03-16 13:42:44 -0600

    likelihood function

--- MCMC_A1.py
+++ MCMC_A1.py
@@ -2,7 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy.random import uniform 
 import scipy.stats as stats
+from scipy.special import gamma, factorial
 
 ### MCMC ASSIGNMENT 1 ###
 #Create an MCMC that draws from Student-t distribution
 
+#The Student-t takes an input of n samples from a normal distribution
+#lets create those samples with a mean of 1 and sig of 1
+samples = np.random.normal(1, 1, 20)
+
+#Now create our likelihood function
+def loglike(x, mu, sig, nu):
+    pi = (gamma((nu+1)/2) / (gamma(nu/2) * np.sqrt(2*np.pi)*sig)) * (1 + (1/nu)*((x - mu)/sig)**2)**(-0.5*(nu+1))
+    return(np.sum(np.log(pi)))
