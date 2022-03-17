df7c0a8a3ab18c56392e4662ac00ca98e98d234e
gfiacco12 <gfiacco12@gmail.com>
2022-03-17 16:14:53 -0600

    messing up my steps

--- MCMC_A1.py
+++ MCMC_A1.py
@@ -17,7 +17,9 @@ def loglike(x):
     sig = 1
     nu = 3
 
-    pi = (gamma((nu+1)/2) / (gamma(nu/2) * np.sqrt(2*np.pi)*sig)) * (1 + (1/nu)*((x - mu)/sig)**2)**(-0.5*(nu+1))
+    a = (gamma((nu+1)/2) / (gamma(nu/2) * np.sqrt(2*np.pi)*sig))
+    b = (1 + (1/nu)*((x - mu)/sig)**2)**(-0.5*(nu+1))
+    pi = a * b
     #now impliment flat priors for our parameters - mu, nu, and sig cannot go below 0
     if mu < 0 or sig < 0.1 or nu < 0:
        return (-1e6)
@@ -25,26 +27,28 @@ def loglike(x):
 
 #create the MCMC
 #inputs are starting point, jump size, number of points until convergence 
-def mcmc(start, alp, conv):
-    x_0 = start #starting point
+def mcmc(alp, conv):
+    x_0 = -2 #starting point
     naccept = 0 #counter for acceptance
     accepted_values = [] #array for all accepted values
     step_iter = [] #array that matches all accepted values
-
+    dist_data = []
 
     #create the jump
     for i in range(0, conv):
 
         #proposal distribution is a gaussian, with jump size alpha, mean (???)
         new_step = x_0 + stats.norm(1, alp).rvs()
-
+        print('current new step', new_step)
         #calculate Hastings ratio - difference of the logs
         y = loglike(new_step) - loglike(x_0)
         #now we want the negative number to be chosen, so compare to 0
         ratio =min(0, y)
-
+        print("the ratio is" , np.exp(ratio))
         #generate a random number between 0 and 1
         u = np.random.uniform(0,1)
+        print("the random number is", u)
+        print("the current jump is", x_0)
         #we need to re-exponentiate the log ratio to compare it to the random number
         if u < np.exp(ratio):
             #accept the point
@@ -54,10 +58,18 @@ def mcmc(start, alp, conv):
             step_iter.append(i)
         else:
             accepted_values.append(x_0)
-    print(naccept)
-    plt.hist(accepted_values, bins=25, density=True)
+        print("the new alpha will be", x_0)
+        print(' -----------')
+        print(' -----------')
+    #if we want to model a student t distribution, and the parameter we are estimating is the "data"
+    #that you put into a student-t dist, then to get out a distribution we need to feed each accepted jump 
+    #through the student-t distribution (our likelihood)
+
+    print(len(accepted_values))
+    plt.hist(accepted_values, bins=20, density=True)
     plt.show()
+
     return(accepted_values, naccept, step_iter)
 
-mcmc(2, 0.1, 100)
+mcmc(0.1, 50)
 
