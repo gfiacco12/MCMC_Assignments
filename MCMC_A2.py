8b90a328c6e620681ec364f70bfaf9bcaee4c81c
gfiacco12 <gfiacco12@gmail.com>
2022-04-04 11:06:38 -0600

    starting new

---
+++ MCMC_A2.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import uniform 
+import scipy.stats as stats
+from scipy.special import gamma, factorial
+import statsmodels.api as sm
