3e621540b9ef8c5be2befe65dc0345aacc9bec67
gfiacco12 <gfiacco12@gmail.com>
2022-04-09 16:43:10 -0600

    I dont even know, man

--- MCMC_A2.py
+++ MCMC_A2.py
@@ -71,51 +71,67 @@ def loglike(dat, param):
 
 
 #Create the Fisher Matrix for all the parameters
-from sympy import symbols, diff, hessian, Function
-def fisher():
+def fisher(dat):
     
-    data = data_generator(1.33, 1000)
     #use finite difference method to calculate derivatives
     params = np.array([1, 3, 1]) #injected paramters for evaulation
     #initializing step size vectors and Hessian matrix
     vector1 = np.zeros(np.size(params))
     vector2 = np.zeros(np.size(params))
     H = np.zeros((np.size(params),np.size(params)))
-    
     h = 0.001
     for i in range(0, len(params)):
         for j in range(0, len(params)):
             if i == j:  #diagonal elements
                 vector1[i] = h
-                H[i][i] = (loglike(data, params+vector1) - 2*loglike(data, params) + loglike(data, params - vector1))/(h**2)
+                H[i][i] = (loglike(dat, params+vector1) - 2*loglike(dat, params) + loglike(dat, params - vector1))/(h**2)
             if i != j:
                 vector1[i] = h 
                 vector2[j] = h
-                H[i][j] = (loglike(data, params + vector1+vector2) - loglike(data, params + vector1-vector2) - loglike(data, params - vector1+vector2) + loglike(data, params - vector1-vector2)) / (4*h**2)
+                H[i][j] = (loglike(dat, params + vector1+vector2) - loglike(dat, params + vector1-vector2) - loglike(dat, params - vector1+vector2) + loglike(dat, params - vector1-vector2)) / (4*h**2)
     fisher = -H
     #now get the eigenvalues. These will tell us the jump directions
     print(fisher)
-    eig = lin.eigvals(fisher)
-    print(eig)
-    return(eig)
-fisher()
+    eigval, eigvec = lin.eig(fisher)
+    print(eigval)
+    print(eigvec)
+    return(eigval, eigvec)
 
 #inputs are data array from above, alpha starting value -based on Fisher Matrix
-def MCMC(dat, alp, conv):
+def MCMC(dat, conv):
     #create proposal - gaussian with new sigma of our choosing
     naccept = 0 #creating a counter for the points accepted
-    accepted_values = []
+    accepted_values_mu = []
+    accepted_values_nu = []
+    accepted_values_sig = []
     step_iter = []
+    eigval, eigvec = fisher(dat)
 
-    #create jump
+    #initialize starting points
+    mu_0 = np.random.normal(0,1)
+    nu_0 = np.random.normal(0,1)
+    sig_0 = np.random.normal(0,1)
+    old_params = [mu_0, nu_0, sig_0]
+
+    #create jumps for all three parameters - vectorize
     for j in range(0, conv):
 
+        #pick which eigendirection to jump towards
+        v = np.random.uniform(0,1)
+        if v <= (1/3):
+            jump_dir = (1 / np.sqrt(eigval[0])) * eigvec[0] * stats.norm(0,1).rvs()
+        if v > (1/3) and v <= (2/3):
+            jump_dir = (1 / np.sqrt(eigval[1])) * eigvec[1] * stats.norm(0,1).rvs()
+        if v > (2/3):
+            jump_dir = (1 / np.sqrt(eigval[2])) * eigvec[2] * stats.norm(0,1).rvs()
+
         #need to create a jump for each parameter
-        new_step = a_0 + stats.norm(0, alp).rvs()
+        parameters = [new_mu, new_nu, new_sig] = old_params + jump_dir
+        #print(np.real(parameters))
 
         #calculate Hastings Ratio - sigma in our likelihood is alpha here
         #ratio of likelihoods = difference of log likelihood
-        y = loglike(new_step, dat) - loglike(a_0, dat)
+        y = loglike(dat, np.real(parameters) )- loglike(dat, np.real(old_params))
 
         #now we want the negative number to be chosen, so compare to 0
         ratio =min(0, y)
@@ -128,10 +144,18 @@ def MCMC(dat, alp, conv):
         if u < np.exp(ratio):
             #accept the point
             naccept = naccept + 1
-            a_0 = new_step
-            accepted_values.append(new_step)
+            [mu_0, nu_0, sig_0] = [new_mu, new_nu, new_sig]
+            accepted_values_mu.append(new_mu)
+            accepted_values_nu.append(new_nu)
+            accepted_values_sig.append(new_sig)
             step_iter.append(j)
         else:
             step_iter.append(j)
-            accepted_values.append(a_0)
+            accepted_values_mu.append(mu_0)
+            accepted_values_nu.append(nu_0)
+            accepted_values_sig.append(sig_0)
+    
+    plt.hist(accepted_values_nu, bins=50, density=True, label="MCMC data")
+    plt.show()
     return()
+MCMC(data_generator(1.33, 1000), 10000)
